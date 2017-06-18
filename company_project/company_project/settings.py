d63cd6e165ed5bbc699b0d84bbf446b3fcfc36b1
rlipi <renatolipi@gmail.com>
2017-06-18 20:20:08 -0300

    fix according to travis tests

--- company_project/company_project/settings.py
+++ company_project/company_project/settings.py
@@ -140,7 +140,7 @@ LOGGING = {
         'default': {
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'logs/app.log',
+            'filename': BASE_DIR+'/logs/app.log',
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'standard',
@@ -148,7 +148,7 @@ LOGGING = {
         'request_handler': {
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'logs/requests.log',
+            'filename': BASE_DIR+'/logs/requests.log',
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'standard',
