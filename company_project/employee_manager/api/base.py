61c7e8e8790280bf65a080401cb58c4dd92b43d8
rlipi <renatolipi@gmail.com>
2017-06-18 19:05:05 -0300

    add logging

--- company_project/employee_manager/api/base.py
+++ company_project/employee_manager/api/base.py
@@ -2,15 +2,20 @@
 from __future__ import unicode_literals
 
 import json
+import logging
 
 from django.http import HttpResponse
 
 from employee_manager.api_tools import check_auth_token, validate_content_type
 
 
+logger = logging.getLogger(__name__)
+
+
 @check_auth_token
 @validate_content_type
 def health(request):
+    logger.debug('health checking')
     response_data = {'content': 'OK'}
     return HttpResponse(
         json.dumps(response_data), content_type="application/json", status=200
